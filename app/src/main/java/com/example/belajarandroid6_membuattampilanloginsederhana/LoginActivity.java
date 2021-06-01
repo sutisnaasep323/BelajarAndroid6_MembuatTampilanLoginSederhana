cee344584ee575abd30a5ff8d55daac8682e8e74
yummicodes <yummicodes@gmail.com>
2021-06-01 13:56:23 +0700

    Menambahkan intent pada button sehingga berpindah ke Main Activity

--- app/src/main/java/com/example/belajarandroid6_membuattampilanloginsederhana/LoginActivity.java
+++ app/src/main/java/com/example/belajarandroid6_membuattampilanloginsederhana/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.belajarandroid6_membuattampilanloginsederhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //method onClick adalah event ketika komponen itu di klik
+                //Actionnya pada kali ini adalah intent. intent = perpindahan activity
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
